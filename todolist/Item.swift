c3f1df8ce934166daadc26aa39ffe7751bb3bf55
yujin <yujinj@inswave.com>
2024-06-10 10:29:48 +0900

    Initial Commit

---
+++ todolist/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  todolist
+//
+//  Created by 정유진 on 6/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
