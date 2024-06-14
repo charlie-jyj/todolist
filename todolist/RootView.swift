efd2e48281bd49101db8a421f69f823461e01776
yujin <yujinj@inswave.com>
2024-06-14 16:47:20 +0900

    change to @Binding instead of @State

--- todolist/RootView.swift
+++ todolist/RootView.swift
@@ -121,8 +121,26 @@ struct RootView: View {
      
      가설 7> 자식 뷰에 넘긴다면 다를 지도? (예전의 장애를 재현하기 위해)
      
-     이전 프로젝트에서 뷰가 갱신되지 않은 이유는 menuList는 @State 이지만 menuItem이 @State 또는 @Binding이 아니었기 때문이다.
-     -> 재현해보도록 하자.
+     가설 8 > 이전 프로젝트에서 뷰가 갱신되지 않은 이유는 menuList는 @State 이지만 menuItem을 넘길 때 @Binding이 아니었기 때문이다.
+     -> 재현해보도록 하자. todoItemView를 만들고 item을 todos[i] 로 넘긴다.
+     결과 > 재현에 성공했다 detail item으로 바뀌지 않음.
+     해결 방안 > @Binding으로 넘긴다
+     
+     의문 > 이전 프로젝트에서 3 -> 0 -> 3 이렇게 했을 때에는 제대로 그려진 이유가 뭐지? @State로 넘기면 다시 안 그려져야 할거 같은데
+     -> List로 감싸지 않으면 다시 그려진다. 이전 프로젝트는 리스트를 쓰지 않고 내가 손수 그림..
+     3 -> 0 되면서 if count > 0 에 의해 뷰가 지워지고 0 -> 3 되면서 뷰가 다시 그려지기 때문에 갱신됩니다.
+     하지만 3 -> 3 에서는 다시 그려지지 않습니다..
+     
+     -> 그러나 List로 감싸면.. 다시 그려지지 않는다! 이런..
+     -> List를 if 문으로 감싸면.. 난 될 것이라고 생각한다.
+     하지만 역시 3 -> 3 에서는 다시 그려지지 않는다. 그렸던 것을 다시 그린다.
+     if count > 0 조건이 달라지지 않았기 때문에 다시 그리지 않는 것
+     
+     결론 > 
+     1. List를 쓰든, 내가 손수 그리든 @Binding으로 넘겨주면 변경 사항이 잘 반영된다.
+     2. Binding으로 안 넘길거면 If 조건문을 잘 사용해서 true -> false || false -> true일 때 업데이트 되는 것을 이용한다.
+     3. View에 id를 주어서 data인 list의 id가 변경되면 다시 그려지도록 유도한다.
+     
      */
     
 //    @ObservedObject var todoManager = TodoManager.shared
@@ -316,59 +334,82 @@ struct TodoListView: View {
         ZStack {
             VStack {
                 HStack {
-//                    NavigationLink(destination: DetailView()) {
-//                        Text("go detail view")
-//                    }
-//                    .padding(.trailing)
-                    Button(action: { toggleDetailView.toggle()}) {
+                    NavigationLink(destination: DetailView()) {
                         Text("go detail view")
                     }
+                    .padding(.trailing)
+//                    Button(action: { toggleDetailView.toggle()}) {
+//                        Text("go detail view")
+//                    }
                 }
                 
-                //        List {
-                //            ForEach(todos.value.indices) { i in
-                if todos.count > 0 {
-                    ForEach(todos.indices) { i in
-                        VStack {
-                            HStack {
-                                Text(todos[i].title)
-                                //                        Text(todos.value[i].title)
-                                Spacer()
-                            }
-                            HStack {
-                                Text(todos[i].content)
-                                //                        Text(todos.value[i].content)
-                                Spacer()
-                                Text(todos[i].date, style: .date)
-                                //                        Text(todos.value[i].date, style: .date)
-                            }
+                // if 문이 true <-> false 여야 view가 update된다. true -> true는 의미 없음
+//                if todos.count > 0 {
+//                    List {
+//                            ForEach(todos.value.indices) { i in
+               
+                        ForEach(todos.indices) { i in
+                            let todo = todos[i]
+//                            TodoItemView(todo: $todos[i])
+                            TodoItemView(todos: $todos,
+                                         todo: todo)
                         }
-                    }
-                }
-                //        } // end of list
+                    
+//                    } // end of list
+//                } // end of if
             }
             .onAppear {
                 print("TodoListView appeared")
             }
             
-            if toggleDetailView {
-                ZStack {
-                    Color.yellow
-                    DetailView(backToList: $toggleDetailView)
-                }
-                .ignoresSafeArea()
-            }
+//            if toggleDetailView {
+//                ZStack {
+//                    Color.yellow
+//                    DetailView(backToList: $toggleDetailView)
+//                }
+//                .ignoresSafeArea()
+//            }
         }
     }
     
 }
 
+struct TodoItemView: View {
+    @Binding var todos: [todoItem] // 내려받은 item으로 갱신됨
+    @State var todo: todoItem // 옛날 item을 물고있음
+//    @Binding var todo: todoItem // 이렇게 하면 됨
+    var body: some View {
+        VStack {
+            HStack {
+                Text(todo.title)
+                Spacer()
+            }
+            
+            TodoContentView(todos: $todos,
+                            todo: todo)
+        }
+    }
+}
+
+struct TodoContentView: View {
+    @Binding var todos: [todoItem]
+    @State var todo: todoItem
+    
+    var body: some View {
+        HStack {
+            Text(todo.content)
+            Spacer()
+            Text(todo.date, style: .date)
+        }
+    }
+}
+
 struct DetailView: View {
-    @Binding var backToList: Bool
+//    @Binding var backToList: Bool
     var body: some View {
             VStack {
                 Button(action: {
-                    backToList.toggle()
+//                    backToList.toggle()
                 }) {
                     Text("Back to List")
                 }
@@ -376,6 +417,13 @@ struct DetailView: View {
                     Button(action: {
                         //                    todoManager.toDoList = []
                         TodoManager.shared.toDoList = []
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                            TodoManager.shared.toDoList = [
+//                                todoItem(title: "AfterEmpty1", content: "Detail1 Content", date: Date()),
+//                                todoItem(title: "AfterEmpty2", content: "Detail2 Content", date: Date()),
+//                                todoItem(title: "AfterEmpty3", content: "Detail3 Content", date: Date())
+//                            ]
+//                        }
                         //                    todoManager.sampleText = "empty button tapped"
                     }) {
                         Text("empty")
